bd42fbbada885c4f3b96b2d471e6ab6be7624c98
Rodrigo Campos <rodrigo.campos@intelitrader.com.br>
2018-10-19 21:05:16 -0300

    Adiciona custom helper

---
+++ AspNetMvcEcommerce/CustomHtmlHelpers/Helpers.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetMvcEcommerce.CustomHtmlHelpers
+{
+    public static class Helpers
+    {
+        public static MvcHtmlString Strong(this HtmlHelper html, string expression)
+        {
+            return new MvcHtmlString("<strong>" + expression + "</strong>");
+        }
+    }
+}
