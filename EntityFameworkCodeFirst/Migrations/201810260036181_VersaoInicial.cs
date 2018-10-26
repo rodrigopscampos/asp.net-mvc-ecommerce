bf5d372198ce1c2b26b2429c1873b60a72137660
Rodrigo Campos <rodrigo.campos@intelitrader.com.br>
2018-10-25 21:38:06 -0300

    Adiciona projeto EntityFramework CodeFirst

---
+++ EntityFameworkCodeFirst/Migrations/201810260036181_VersaoInicial.cs
@@ -0,0 +1,42 @@
+namespace TryEntityFamework.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class VersaoInicial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Blogs",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Content = c.String(),
+                        BlogId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Blogs", t => t.BlogId, cascadeDelete: true)
+                .Index(t => t.BlogId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Posts", "BlogId", "dbo.Blogs");
+            DropIndex("dbo.Posts", new[] { "BlogId" });
+            DropTable("dbo.Posts");
+            DropTable("dbo.Blogs");
+        }
+    }
+}
