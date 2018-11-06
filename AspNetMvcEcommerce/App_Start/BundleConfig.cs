98b9d780b6fbd68ecffd14a1bdd36207d760f8a0
rodrigo campos <rodrigo.campos@info.local>
2018-11-06 21:13:01 -0200

    correcoes de bundle e connectionstring

--- AspNetMvcEcommerce/App_Start/BundleConfig.cs
+++ AspNetMvcEcommerce/App_Start/BundleConfig.cs
@@ -11,13 +11,16 @@ namespace AspNetMvcEcommerce
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.bundle.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-grid.css",
+                      "~/Content/bootstrap-reboot.css",
                       "~/Content/site.css"));
 
-            //BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
