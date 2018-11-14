d4cb51ff99bb10355a9ceb6598b76c11b3aab0c3
rpscampos <ro.rdg@hotmail.com>
2018-11-14 18:32:14 -0200

    Adiciona paginas de erros customizada

--- AspNetMvcEcommerce/Global.asax.cs
+++ AspNetMvcEcommerce/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -12,6 +13,12 @@ namespace AspNetMvcEcommerce
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegistraBundlers(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var ex = Server.GetLastError();
+            Console.WriteLine(ex);
         }
     }
 }
