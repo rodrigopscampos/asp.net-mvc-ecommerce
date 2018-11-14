139c1da9d7b95664c9c28b0fd57dd1361cc5daf6
rpscampos <ro.rdg@hotmail.com>
2018-11-14 21:27:11 -0200

    remove bootstrap do pacote nuget e adiciona do cdn

--- AspNetMvcEcommerce/App_Start/BundleConfig.cs
+++ AspNetMvcEcommerce/App_Start/BundleConfig.cs
@@ -9,18 +9,8 @@ namespace AspNetMvcEcommerce
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootstrap.bundle.js",
-                      "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      //"~/Content/bootstrap-grid.css",
-                      //"~/Content/bootstrap-reboot.css",
                       "~/Content/site.css"));
-
-            BundleTable.EnableOptimizations = true;
         }
     }
 }
