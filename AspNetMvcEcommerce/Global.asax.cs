38dc6894931dcd942c776b5f31b0aef87fe9d0e7
rpscampos <ro.rdg@hotmail.com>
2018-11-14 18:35:12 -0200

    Remove area admin

--- AspNetMvcEcommerce/Global.asax.cs
+++ AspNetMvcEcommerce/Global.asax.cs
@@ -9,7 +9,6 @@ namespace AspNetMvcEcommerce
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegistraBundlers(BundleTable.Bundles);
