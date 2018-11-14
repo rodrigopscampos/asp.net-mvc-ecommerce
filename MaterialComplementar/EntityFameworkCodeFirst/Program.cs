7165a5c2bb60770c3eb811089f97b6514a33a391
rpscampos <ro.rdg@hotmail.com>
2018-11-14 21:51:23 -0200

    Adiciona material complementar

---
+++ MaterialComplementar/EntityFameworkCodeFirst/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EntityFameworkCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Blogcontext())
+            {
+                var blog = new Blog { Name = "blog nome" };
+                context.Blogs.Add(blog);
+                context.SaveChanges();
+
+                foreach (var item in context.Blogs)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
